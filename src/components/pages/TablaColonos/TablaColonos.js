0ce1d622bdca6c88c7a70ee1a0e24e53cd870a6b
Ernest <ridik_ecp@hotmail.com>
2020-05-13 21:46:23 -0500

    Prototype 1
    GUI prot 1

--- src/components/pages/TablaColonos/TablaColonos.js
+++ src/components/pages/TablaColonos/TablaColonos.js
@@ -1,25 +1,39 @@
 import React, { Component } from "react";
-import { Button, Form, Input } from "semantic-ui-react";
+import { Label, Select, Button, Form, Input } from "semantic-ui-react";
 import "./TablaColonos.scss";
 class TablaColonos extends Component {
   render() {
     var fecha = new Date();
+    const moni = Math.round(Math.random() * 500000);
     const options = [
       { key: "1", value: "Calle 1", text: "Luis cortazar" },
       { key: "2", value: "Calle 2", text: "Dr. Mora" },
     ];
     return (
       <div className="tableColonos">
-        <h1>Portones de la hacienda</h1>
-        <h2>Registro de pagos</h2>
-        <Form widths="equal" className="formulario">
-          <Form.Select
-            className="inp"
+        <h2>Portones de la hacienda</h2>
+        <Input
+          className="count"
+          value={moni}
+          size="mini"
+          labelPosition="right"
+          type="text"
+          placeholder="Amount"
+        >
+          <Label basic>$</Label>
+          <input />
+        </Input>
+        <h3>Registro de pagos</h3>
+        <Form className="formulario">
+          <Select
+            className="sel"
             fluid
             options={options}
             placeholder="Domicilio"
           />
           <Input
+            label={{ icon: "address card" }}
+            labelPosition="left corner"
             className="inp"
             type="text"
             name="email"
@@ -27,16 +41,20 @@ class TablaColonos extends Component {
           ></Input>
 
           <Input
+            label={{ icon: "money bill alternate outline" }}
+            labelPosition="left corner"
             className="inp"
             type="text"
             name="email"
             placeholder="Monto"
           ></Input>
           <Input
+            label={{ icon: "calendar" }}
+            labelPosition="left corner"
             className="inp"
             type="text"
             name="email"
-            placeholder={
+            value={
               fecha.getDay() +
               "/" +
               (fecha.getMonth() + 1) +
@@ -50,7 +68,6 @@ class TablaColonos extends Component {
             }
             readOnly
           ></Input>
-          <br />
           <Button size="medium" className="btn">
             Aceptar
           </Button>
