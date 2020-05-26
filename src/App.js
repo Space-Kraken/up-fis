3b5cf9720be9a1c30e4f5ccb35e721376662278c
David Guerrero <17030752@itcelaya.edu.mx>
2020-05-25 20:48:20 -0500

    implementacion de firebase
    Creacion de las funciones Create , Read del CRUD de una base de datos ademas de la implementacion de firebase a la app web y tambien se instalaron los modulos bootstrap reactstrap y firebase.

--- src/App.js
+++ src/App.js
@@ -4,13 +4,16 @@ import Footer from "./components/Frame/Footer";
 import Titlebar from "./components/Frame/Titlebar";
 import TablaColonos from "./components/pages/TablaColonos/TablaColonos";
 import "./App.scss";
-
+import Todos from "./components/Todos"
 function App() {
+  
   return (
     <div className="content">
       <Titlebar />
       <div className="main-dev">
         <TablaColonos />
+        <br/>
+        <Todos/>
       </div>
       <Footer />
     </div>
